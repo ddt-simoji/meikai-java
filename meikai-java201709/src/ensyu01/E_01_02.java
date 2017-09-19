9e7857872ae97dbc9a02cf014e73f8353763cd07
ddt-simoji <ddt.simoji@gmail.com>
2017-09-19 21:07:32 +0900

    演習1-2のコミットです。

---
+++ meikai-java201709/src/ensyu01/E_01_02.java
@@ -0,0 +1,15 @@
+package ensyu01;
+
+/* 明解Java
+ * 演習 1-2
+ * 各行に１文字ずつ名前を表示するプログラムを作成せよ。
+ */
+
+public class E_01_02 {
+
+	public static void main(String[] args) {
+		System.out.println("下\n地\n愛\n香");
+
+	}
+
+}
