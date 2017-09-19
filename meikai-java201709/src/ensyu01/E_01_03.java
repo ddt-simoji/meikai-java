0ae97c2917c6aa4f9af4b4ed00dae842997847e9
ddt-simoji <ddt.simoji@gmail.com>
2017-09-19 21:08:11 +0900

    演習1-3のコミットです。

---
+++ meikai-java201709/src/ensyu01/E_01_03.java
@@ -0,0 +1,16 @@
+package ensyu01;
+
+/* 明解Java
+ * 演習 1-3
+ * 各行に１文字ずつ名前を表示するプログラムを作成せよ。
+ * 姓と名の間は１行あけることとし、自分の名前を表示すること。
+ */
+
+public class E_01_03 {
+
+	public static void main(String[] args) {
+		System.out.println("下\n地\n\n愛\n香");
+
+	}
+
+}
