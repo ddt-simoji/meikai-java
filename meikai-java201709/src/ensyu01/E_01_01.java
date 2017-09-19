bc43d5d30b2670aff21d0fc4b74a972110ad141e
ddt-simoji <ddt.simoji@gmail.com>
2017-09-19 21:06:48 +0900

    演習1‐1のコミットです。

---
+++ meikai-java201709/src/ensyu01/E_01_01.java
@@ -0,0 +1,20 @@
+package ensyu01;
+
+/* 明解Java
+ * 演習 1-1
+ * プログラム中の分の終端を示すセミコロン；が欠如しているとどうなるか。
+ * プログラムをコンパイルして実行せよ。
+ */
+
+public class E_01_01 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+		// ;がないときの挙動
+		// 構文エラーでコンパイルに失敗します。
+		// System.out.println("test")
+
+	}
+
+}
