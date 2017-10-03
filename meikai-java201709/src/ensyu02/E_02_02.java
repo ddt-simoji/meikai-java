8d24215c39669d89e5cfd71a5f3b04eab91b8d6f
ddt-simoji <ddt.simoji@gmail.com>
2017-10-03 19:37:14 +0900

    E_02_02のコミットを修正しました。 ・コメントの位置を訂正しました。

--- meikai-java201709/src/ensyu02/E_02_02.java
+++ meikai-java201709/src/ensyu02/E_02_02.java
@@ -9,13 +9,13 @@ package ensyu02;
 public class E_02_02 {
 //3つのint型変数に値を代入し、合計と平均を求める処理。
 	public static void main(String[] args) {
-		int x = 10;
-		int y = 20;
-		int z = 30;
+		int num1 = 10;  //
+		int num2 = 20;
+		int num3 = 30;
 			//計算し、合計を表示する。
-			System.out.println("合計：" + ( x + y + z));
+			System.out.println("合計：" + ( num1 + num2 + num3));
 			//計算し、平均を表示する。
-			System.out.println("平均：" + ( x + y + z)/3);
+			System.out.println("平均：" + ( num1 + num2 + num3)/3);
 	}
 
 }
